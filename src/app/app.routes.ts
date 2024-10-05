16b27a5da86491afc27f481d89972b33f5fe1b1e
Aman Singh <137636775+code-with-aman111@users.noreply.github.com>
2024-10-05 09:38:28 -0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ProjectEmployeeComponent } from './pages/project-employee/project-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+           {
+             path:'dashboard',
+            component:DashboardComponent,
+        },
+        {
+            path:'employee',
+            component:EmployeeComponent
+        },
+        {
+         path:'projects',
+         component:ProjectComponent
+        },
+        {
+         path:'project-employee',
+         component:ProjectEmployeeComponent
+        }
+        ]
+    }
+];
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
